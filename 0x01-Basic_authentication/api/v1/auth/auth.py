900e671fe0847e9e2a63721ca51bb8c88e61b6a2
Emmanuel Onyekachi <emmanuelonyekachi04122000@gmail.com>
2024-11-15 16:56:52 +0100

    Basic authentiaction

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+Authentication class for the API
+"""
+
+from flask import request
+from typing import TypeVar, List
+
+
+class Auth():
+    """
+    Base class definition for the API authentication.
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Check if the path requires authentication"""
+        if not path or not excluded_paths:
+            return True
+        if path[(len(path) - 1)] == '/':
+            if path in excluded_paths:
+                return False
+            elif path[:-1] in excluded_paths:
+                return False
+        else:
+            if path in excluded_paths:
+                return False
+            elif (path + '/') in excluded_paths:
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """Check if the authorization object is present."""
+        if not request:
+            return None
+        if not request.headers.get('Authorization', None):
+            return None
+        return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Returns the current user who sent the request."""
+        return None
